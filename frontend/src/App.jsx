ecc70caedeb3d409de912d618220866a2b32a1ea
alandiegobiz <adiegobiz@gmail.com>
2021-01-18 12:05:26 -0300

    Create frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+//Dependências e bibliotecas
+import React from 'react'
+
+//Componentes
+import Home from './pages/Home'
+
+//Assets - Imagens, CSS
+import './assets/styles/global.css';
+
+const App = () => {
+    return (
+        <>
+            <Home />
+        </>
+    )
+}
+
+export default App
---
+++ frontend/src/pages/Home/index.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import Animation from '../../components/Animation';
+import CardCourse from '../../components/CardCourse';
+
+import './styles.css';
+
+import yoda from '../../assets/images/characters/yoda.png'
+import separator from '../../assets/images/separator.svg'
+import courseReactRedux from '../../assets/images/courses/react-redux.jpg';
+import coursePhp from '../../assets/images/courses/php.jpg';
+import courseFlutter from '../../assets/images/courses/flutter.jpg';
+
+
+const Home = () => {
+    return (
+        <>
+
+            <Animation />
+
+            <div className="section-1">
+                <div className="container">
+                    <header className="header">
+                        <h2>Luke Code</h2>
+                        <nav>
+                            <button>FAZER LOGIN</button>
+                            <button>INCREVA-SE</button>
+                        </nav>
+                    </header>
+                    <main>
+                        <section>
+                            <h1>Se torne um mestre <br/> da programação!</h1>
+                            <img src={separator} alt=""/>
+                            <p>
+                                De o próximo passo para o seu futuro, <br/>
+                                que a força esteja com você.
+                            </p>
+                        </section>
+                        <section>
+                            <img src={yoda} alt="Yoda - Star Wars"/>
+                        </section>
+                    </main>
+                </div>
+            </div>
+            <div className="section-2">
+                <div className='container'>
+                    <h2>+3 Cursos Completos</h2>
+                    <p>
+                        Na Luke Code, você tem acesso a 3 cursos gratuitos, basta se cadastrar na plataforma.
+                    </p>
+                    <section className="card-container">
+                        <CardCourse image={courseReactRedux} description="Desenvolvimento front-end React e Redux">
+                            Desenvolvimento front-end<br />React e Redux
+                        </CardCourse>
+                        <CardCourse image={coursePhp} description="PHP 7 - Trabalhando com PSRs">
+                            PHP 7 - Trabalhando com PSRs
+                        </CardCourse>
+                        <CardCourse image={courseFlutter} description="Desenvolvimento IOS e Android com Flutter">
+                            Desenvolvimento IOS e Android <br /> com Flutter
+                        </CardCourse>
+                    </section>
+                    <button>
+                        INCREVA-SE
+                    </button>
+                </div>
+            </div>
+            <div className="section-3">
+                <div className="container">
+                    <h2>O que oferecemos para você</h2>
+                    <section className="categories-container">
+                        
+                        <div className="category-card">
+                            <div className="category-card-image">
+                                {/*<img src="" alt=""/>*/}
+                            </div>
+                            <div className="category-card-content">
+                                <h4>BACK-END</h4>
+                                <p>22 Cursos</p>
+                            </div>
+                        </div>
+
+
+                    </section>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Home;
